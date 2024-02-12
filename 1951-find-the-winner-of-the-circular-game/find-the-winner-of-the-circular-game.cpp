e7c57828c84c5e532ac2ecdebdbc06774a1941ed
Puru Sachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-02-12 22:47:42 +0530

    Time: 2 ms (75.51%) | Memory: 7.1 MB (42.43%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,58 +1,15 @@
 class Solution {
 public:
+
+    int solve(int n, int k) {
+        if(n == 1) return 0;
+
+        return (solve(n-1,k)+k)%n;
+    }
+
     int findTheWinner(int n, int k) {
-        if(n == 1) return 1;
-        int i = 0;
+        int ans = solve(n,k) + 1;
 
-        vector<int> vec(n);
-        iota(vec.begin(), vec.end(), 1);
-
-        int lost = 0;
-        while(1) {
-            // cout << "before count: " << i + 1 << endl;
-            i = i % n;
-            int count = 1;
-            while(count < k) {
-                if(vec[i] == -1) {
-                    i++;
-                    i = i % n;
-                    continue;
-                } else {
-                    count++;
-                    i++;
-                    i = i % n;
-                }
-            }
-
-            while(vec[i] == -1) {
-                i++;
-                i = i%n;
-            }
-
-            // cout << "after count: " << i + 1 << endl;
-
-            // cout << "OUT: " << vec[i] << endl;
-            vec[i] = -1;
-            lost++;
-
-            // cout << "Starting from: " << i + 1 << endl;
-
-
-            // for(auto num : vec) {
-            //     cout << num << " ";
-            // }
-            // cout << "\n\n";
-
-            if(lost == n-1) break;
-
-
-        }
-
-        for(auto num : vec) {
-            // cout << num << " ";
-            if(num != -1) return num;
-        }
-
-        return -1;
+        return ans;
     }
 };
