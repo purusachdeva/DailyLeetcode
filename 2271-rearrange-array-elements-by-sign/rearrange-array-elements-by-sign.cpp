ebf07dd2ef381a1ecbefc4746a9df2d148ee3735
Puru Sachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-02-14 10:33:37 +0530

    Time: 145 ms (77.60%) | Memory: 126.4 MB (42.43%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -2,20 +2,38 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
 
-        vector<int> positive;
-        vector<int> negative;
-        for(auto i : nums) {
-            if(i > 0) positive.push_back(i);
-            else negative.push_back(i);
+        // vector<int> positive;
+        // vector<int> negative;
+        // for(auto i : nums) {
+        //     if(i > 0) positive.push_back(i);
+        //     else negative.push_back(i);
+        // }
+
+        // int a = 0, b = 0;
+
+        // for(int i = 0; i < nums.size(); i++){
+        //     if(i % 2 == 0) nums[i] = positive[a++];
+        //     else nums[i] = negative[b++];
+        // }
+
+        // return nums;
+
+
+        vector<int> ans(nums.size());
+
+        int pos = 0, neg = 1;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] > 0) {
+                ans[pos] = nums[i];
+                pos += 2;
+            } else {
+                ans[neg] = nums[i];
+                neg += 2;
+            }
         }
 
-        int a = 0, b = 0;
-
-        for(int i = 0; i < nums.size(); i++){
-            if(i % 2 == 0) nums[i] = positive[a++];
-            else nums[i] = negative[b++];
-        }
-
-        return nums;
+        return ans;
     }
+
 };
