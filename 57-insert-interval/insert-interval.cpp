4391611c63bac757f324da5e9b0134eb60113fdd
Puru Sachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-03-18 00:35:54 +0530

    Time: 18 ms (17.35%) | Memory: 20.5 MB (58.08%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,38 +1,27 @@
-bool cmp(vector<int> a, vector<int> b) {
-    return a[0] < b[0];
-}
-    
-
 class Solution {
 public:
-    void checkCondition(vector<vector<int>>& intervals, int i) {
-        if(i == intervals.size() - 1) return;
-
-        if(intervals[i][1] < intervals[i+1][0]) {
-            checkCondition(intervals, i + 1);
-        }
-            
-        else {
-            if(intervals[i + 1][1] > intervals[i][1]) {
-                intervals[i][1] = intervals[i+1][1];
-            }
-            
-            for(int j = i + 1; j < intervals.size() - 1; j++) 
-                intervals[j] = intervals[j+1];
-                
-            intervals.pop_back();
-
-            checkCondition(intervals, i );
-        }
-    }
-    
-    
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back(newInterval);
+        vector<vector<int>> merged;
 
-        sort(intervals.begin(), intervals.end(), cmp);
+        int i = 0;
 
-        checkCondition(intervals, 0);
-        return intervals;
+        while(i < intervals.size() && newInterval[0] > intervals[i][1]) {
+            merged.push_back(intervals[i]);
+            i++;
+        }
+
+        while(i < intervals.size() && intervals[i][0] <= newInterval[1]) {
+            newInterval = {min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])};
+            i++;
+        }
+
+        merged.push_back(newInterval);
+
+        while(i < intervals.size()) {
+            merged.push_back(intervals[i]);
+            i++;
+        }
+
+        return merged;
     }
 };
