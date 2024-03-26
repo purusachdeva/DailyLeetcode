ca2cb3230213b5a918fa74d8885d750ab373d87a
Puru Sachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-03-27 00:26:26 +0530

    Time: 34 ms (93.31%) | Memory: 43.6 MB (55.21%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -5,8 +5,6 @@ public:
             if(nums[i] <= 0) nums[i] = INT_MAX;
         }
 
-        // Indexing to be used -> 1,2,3,4,5
-
         for(int i = 0; i < nums.size(); i++) {
             int index = abs(nums[i]) - 1;
             if(index < nums.size() && nums[index] > 0) {
