6dd190da479bb19b6006ef36e3b88f7ca8137f0d
Puru Sachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-07-14 16:04:25 +0530

    Time: 7 ms (75.69%) | Memory: 8.9 MB (41.00%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        int freq[256] = {0};
 
-        return s == t;
+        for (int i = 0; i < s.size(); i++)
+            freq[s[i]]++;
+
+        for (int i = 0; i < t.size(); i++)
+            freq[t[i]]--;
+
+        for (auto i : freq)
+            if (i != 0) return false;
+
+        return true;
     }
 };
