3dc7ae9448a6b75dfeafa4f62c8007b171f96528
Puru Sachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-07-18 01:52:27 +0530

    Time: 27 ms (61.78%) | Memory: 40.3 MB (13.08%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -6,13 +6,14 @@ public:
         vector<int> left(nums.size(), 1);
         vector<int> right(nums.size(), 1);
 
-        for(int i = 1, j = nums.size() - 2; i < nums.size(); i++, j--) {
-            left[i] = left[i-1]*nums[i-1];
-            right[j] = right[j+1]*nums[j+1];
+        for(int i = 1; i < nums.size(); i++) {
+            ans[i] = ans[i-1]*nums[i-1];
         }
-
-        for(int i = 0; i < ans.size(); i++) {
-            ans[i] = left[i] * right[i];
+        
+        int product = 1;
+        for(int j = nums.size() - 1; j >= 0; j--) {
+            ans[j] = ans[j]*product;
+            product *= nums[j];
         }
 
         return ans;
