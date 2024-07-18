e23a856c658c1a999cd82425020ac66fa45c1f28
Puru Sachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-07-19 01:29:06 +0530

    Time: 20 ms (37.61%) | Memory: 24.8 MB (29.24%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        // Horizontal
+        for(auto i : board) {
+            unordered_map<char, bool> mp; 
+            for(auto j : i) {
+                if(j == '.') continue;
+                if(mp[j]) return false;
+                mp[j] = true;
+            }
+        }
+
+        // Vertical
+        for(int i = 0; i < board.size(); i++) {
+            unordered_map<char, bool> mp;
+            for(int j = 0; j < board.size(); j++) {
+                if(board[j][i] == '.') continue;
+                if(mp[board[j][i]]) return false;
+                mp[board[j][i]] = true;    
+            }
+        }
+
+        // Box
+        vector<pair<int, int>> startPoints;
+
+        for(int i = 0; i < board.size(); i+=3) {
+            for(int j = 0; j < board.size(); j+=3) {
+                startPoints.push_back(make_pair(i, j));
+            }
+        }
+
+        for(auto box : startPoints) {
+            int i = box.first;
+            int j = box.second;
+
+            unordered_map<char, int> mp;
+            for(int a = 0; a < 3; a++) {
+                for(int b = 0; b < 3; b++) {
+                    if(board[i + a][j + b] == '.') continue;
+                    if(mp[board[i + a][j + b]]) return false;
+                    mp[board[i + a][j + b]] = true;
+                }
+            }
+        }
+
+        return true;
+    }
+};
