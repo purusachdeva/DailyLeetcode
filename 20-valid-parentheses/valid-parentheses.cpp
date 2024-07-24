3eaa9093420f10b1bdf80a2eceb752a6d81695ca
Puru Sachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-07-25 00:50:37 +0530

    Time: 5 ms (11.94%) | Memory: 8 MB (21.61%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -4,24 +4,23 @@ public:
         stack<char> st;
 
         for(auto i : s) {
-            if(i == '(' || i == '[' || i == '{') {
+            if(i == '(' || i == '{' || i == '[') {
                 st.push(i);
-            } 
-            
-            else if (st.empty()) return false;
-
-            else if(i == ')') {
-                if(st.top() == '(') st.pop();
-                else return false;
-            } else if(i == ']') {
-                if(st.top() == '[') st.pop();
-                else return false;
-            } else if(i == '}') {
-                if(st.top() == '{') st.pop();
-                else return false;
+                cout << i << " ";
             }
+
+            else if(
+                    (st.empty()) || 
+                    (i == ')' && st.top() != '(') || 
+                    (i == '}' && st.top() != '{') || 
+                    (i == ']' && st.top() != '[')
+                ) {
+                    return false;
+                }
+
+            else st.pop();
         }
 
-        return st.empty();
+        return st.empty() ;
     }
 };
