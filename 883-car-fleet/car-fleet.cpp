b22a56aeafc8fc3817ce3bcfe7d06d22343f45af
Puru Sachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-07-27 15:44:31 +0530

    Time: 153 ms (30.33%) | Memory: 77.8 MB (93.02%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -3,35 +3,25 @@ public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
         int n = position.size();
 
+        vector<int> indices(n);
+        for (int i = 0; i < n; ++i) indices[i] = i;
 
-        std::vector<int> indices(position.size());
-        for (size_t i = 0; i < indices.size(); ++i) {
-            indices[i] = i;
-        }
-
-        // Sorting indices based on the values in the position vector
-        std::sort(indices.begin(), indices.end(), [&](int a, int b) {
+        sort(indices.begin(), indices.end(), [&](int a, int b) {
             return position[a] < position[b];
         });
 
-        // Creating temporary sorted vectors
-        std::vector<int> sorted_position(position.size());
-        std::vector<int> sorted_speed(speed.size());
-
-        for (size_t i = 0; i < indices.size(); ++i) {
-            sorted_position[i] = position[indices[i]];
+        vector<int> sorted_speed(speed.size());
+        sort(position.begin(), position.end());
+        
+        for (int i = 0; i < indices.size(); ++i)
             sorted_speed[i] = speed[indices[i]];
-        }
 
-        position = sorted_position;
         speed = sorted_speed;
-
 
         int fleets = 1;
         float timeReq = timeReq = (target - position[n - 1])/float(speed[n - 1]);
 
         for(int i = n - 2; i >= 0; i--) {
-
             float timeReqPrev = (target - position[i])/float(speed[i]);
 
             if (timeReqPrev > timeReq) {
