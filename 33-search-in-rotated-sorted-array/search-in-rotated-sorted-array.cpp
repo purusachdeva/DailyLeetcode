5578329b8ca85c9bfca3b6f7d9379881cde3619d
Puru Sachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-07-29 00:16:34 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (20.23%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,52 +1,21 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& nums, int target, int i, int j) {
-
-        while (i <= j) {
-            int mid = (i + j) / 2;
-
-            if (nums[mid] == target) return mid;
-            if (nums[mid] < target) i = mid + 1;
-
-            else j = mid - 1;
-        }
-
-        int mid = (i + j) / 2;
-        if (nums[mid] == target) return mid;
-
-        return -1;
-    }
-
     int search(vector<int>& nums, int target) {
         int i = 0, j = nums.size() - 1;
 
-        // if (nums[i] == target) return i;
-        // if (nums[j] == target) return j;
-
         while (i <= j) {
-
-            if (nums[i] == target) return i;
-            if (nums[j] == target) return j;
-
             int m = (i + j) / 2;
-            if(target == nums[m]) return m;
+            if (nums[m] == target) return m;
 
-            if (target > nums[j] && target > nums[i]) {
-                i++;
-                j--;
+            if (nums[i] <= nums[m]) {
+                if(nums[i] <= target && target < nums[m]) j = m - 1;
+                else i = m + 1;
             }
 
-            else if (target > nums[j] && target < nums[i]) {
-                return -1;
-            }
-
-            else if (target < nums[j] && target > nums[i]) {
-                return binarySearch(nums, target, i, j);
-            }
-
-            else if (target < nums[j] && target < nums[i]) {
-                i++;
-                j--;
+            else {
+                if (nums[m] < target && target <= nums[j]) {
+                    i = m + 1;
+                } else j = m - 1;
             }
         }
 
