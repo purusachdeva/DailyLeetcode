dc7188708987410f11a9f3dbf8e5493c6a067d41
Puru Sachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-08-04 22:11:52 +0530

    Time: 218 ms (17.48%) | Memory: 85.9 MB (19.56%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,15 +1,26 @@
 class Solution {
     struct Compare {
         bool operator()(const std::vector<int>& a, const std::vector<int>& b) {
-            return (a[0] * a[0] + a[1] * a[1]) > (b[0] * b[0] + b[1] * b[1]);
+            return (a[0] * a[0] + a[1] * a[1]) < (b[0] * b[0] + b[1] * b[1]);
         }
     };
+
+    int dist(vector<int> v) {
+        return v[0]*v[0] + v[1]*v[1];
+    }
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         priority_queue<vector<int>, vector<vector<int>>, Compare> pq;
 
         for(auto i : points) {
-            pq.push(i);
+            if (pq.size() >= k) {
+                if (dist(pq.top()) > dist(i)) {
+                    pq.pop();
+                    pq.push(i);
+                }
+            }
+
+            else pq.push(i);
         }
 
         vector<vector<int>> ans;
