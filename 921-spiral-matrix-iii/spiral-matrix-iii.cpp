89c31ce9fa41ea5e3b2fd316f570e594db76dbc3
Puru Sachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-08-08 18:16:12 +0530

    Time: 11 ms (42.57%) | Memory: 13.8 MB (23.17%) - LeetSync

---
+++ 921-spiral-matrix-iii/spiral-matrix-iii.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        int n = rows*cols;
+        int i = 1;
+        int cCurr = cStart;
+        int rCurr = rStart;
+
+        vector<vector<int>> ans;
+        int k = -1;
+        while (n) {
+            // right
+            for(int j = 0; j < i; j++) {
+                if (cCurr < cols && rCurr < rows && cCurr > -1 && rCurr > -1)  {
+                    ans.push_back({rCurr, cCurr});
+                    n--;
+                }
+                cCurr++;
+            }
+
+            if (k % 2 == 0) i++;
+            k++;
+
+            // down
+            for(int j = 0; j < i; j++) {
+                if (cCurr < cols && rCurr < rows && cCurr > -1 && rCurr > -1)  {
+                    ans.push_back({rCurr, cCurr});
+                    n--;
+                }
+                rCurr++;
+            }
+
+            if (k % 2 == 0) i++;
+            k++;
+
+            // left
+            for(int j = 0; j < i; j++) {
+                if (cCurr < cols && rCurr < rows && cCurr > -1 && rCurr > -1)  {
+                    ans.push_back({rCurr, cCurr});
+                    n--;
+                }
+                cCurr--;
+            }
+
+            if (k % 2 == 0) i++;
+            k++;
+
+            // up
+            for(int j = 0; j < i; j++) {
+                if (cCurr < cols && rCurr < rows && cCurr > -1 && rCurr > -1)  {
+                    ans.push_back({rCurr, cCurr});
+                    n--;
+                }
+                rCurr--;
+            }
+
+            if (k % 2 == 0) i++;
+            k++;
+        }
+
+        return ans;
+    }
+};
