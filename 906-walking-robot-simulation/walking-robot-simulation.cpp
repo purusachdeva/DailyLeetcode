f566123f4457753d8ec1f65f3c687d71855f9038
Puru Sachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-09-04 19:41:07 +0530

    Time: 68 ms (75.14%) | Memory: 37.4 MB (76.80%) - LeetSync

---
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int x = 0, y = 0, dir = 0;
+        int maxDist = 0;
+
+        set<pair<int, int>> obstacleSet;
+        for (auto& obs : obstacles) {
+            obstacleSet.insert({obs[0], obs[1]});
+        }
+
+        for (auto command : commands) {
+            if (command == -2) {
+                dir = (dir + 3) % 4; // Turn left
+            } else if (command == -1) {
+                dir = (dir + 1) % 4; // Turn right
+            } else {
+                for (int i = 0; i < command; ++i) {
+                    int newX = x + directions[dir].first;
+                    int newY = y + directions[dir].second;
+
+                    if (obstacleSet.find({newX, newY}) == obstacleSet.end()) {
+                        x = newX;
+                        y = newY;
+                        maxDist = max(maxDist, x * x + y * y);
+                    } else {
+                        break; // Stop moving in this direction due to an obstacle
+                    }
+                }
+            }
+        }
+
+        return maxDist;
+    }
+};
