cfaec24cc06560df0596a4a676173eb78f4b6834
Puru Sachdeva <68560995+purusachdeva@users.noreply.github.com>
2024-09-09 15:14:41 +0530

    Time: 29 ms (51.70%) | Memory: 25.2 MB (35.78%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,17 +3,15 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> m;
 
-        for(auto s : strs) {
-            vector<int> count(26, 0);
-            for(auto c : s) count[c - 'a']++;
-
-            string key = "";
-            for(auto i : count) key += (to_string(i) + ",");
-            m[key].push_back(s);
+        for (auto s : strs) {
+            string sorted = s;
+            sort(sorted.begin(), sorted.end());
+            m[sorted].push_back(s);
         }
 
         vector<vector<string>> ans;
-        for(auto i : m) {
+
+        for (auto i : m) {
             ans.push_back(i.second);
         }
 
